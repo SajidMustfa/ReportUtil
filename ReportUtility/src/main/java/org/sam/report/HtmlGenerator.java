06d9d719fc5e069b2207115111fe3d418e4b1337
MD Sajid <md.sajidmustfa@gmail.com>
2015-12-07 01:49:29 +0530

    Commit

---
+++ ReportUtility/src/main/java/org/sam/report/HtmlGenerator.java
@@ -0,0 +1,98 @@
+package org.sam.report;
+
+
+
+	import java.io.File;
+	import java.io.IOException;
+	import java.util.List;
+
+	import org.apache.commons.io.FileUtils;
+	//import org.pack.data.ReportData;
+
+	import com.webfirmframework.wffweb.interpreter.CodeExecutor;
+	import com.webfirmframework.wffweb.tag.html.AbstractHtml;
+	import com.webfirmframework.wffweb.tag.html.Body;
+	import com.webfirmframework.wffweb.tag.html.Div;
+	import com.webfirmframework.wffweb.tag.html.H5;
+	import com.webfirmframework.wffweb.tag.html.Head;
+	import com.webfirmframework.wffweb.tag.html.Html;
+	import com.webfirmframework.wffweb.tag.html.Table;
+	import com.webfirmframework.wffweb.tag.html.Td;
+	import com.webfirmframework.wffweb.tag.html.Tr;
+	import com.webfirmframework.wffweb.tag.html.attribute.global.Style;
+	import com.webfirmframework.wffweb.tag.htmlwff.Blank;
+
+	public class HtmlGenerator {
+
+		public void generateReport(List<ReportData> reportDatas) {
+			Html html = new Html(null) {
+
+				Head head = new Head(this);
+				Body body = new Body(this) {
+					
+				final Style paragraphStyle = new Style("background:cyan");
+
+					Div div = new Div(this, paragraphStyle) {
+						Table table = new Table(this, null);
+					
+	                    CodeExecutor executor = new CodeExecutor(this) {
+
+	                        @Override
+	                        public void execute(AbstractHtml base) {
+	                        	
+	        					Tr headerRow = new Tr(table, null) {
+	        						
+	        						Td rowData1 = new Td(this, null) {
+	        	                    H5 h1 = new H5(this) {
+	        	                        Blank headerContent = new Blank(this,
+	        	                        		"Job Name");
+	        	                    };
+	        					};
+	        	                    
+	        					Td rowData2 = new Td(this, null) {
+	        	                    H5 h2 = new H5(this) {
+	        	                        Blank headerContent = new Blank(this,
+	        	                        		"Status");
+	        	                    };
+	        					};
+	        						
+	        				};
+	                        	
+	                        	for(ReportData data : reportDatas) {
+	            					Tr row = new Tr(table, null) {
+	            						
+	            						Td rowData1 = new Td(this, null) {
+	            							Blank name = new Blank(this,
+	            									data.getName());
+	            					};
+	            					Td rowData2 = new Td(this, null) {
+	            						Blank status = new Blank(this,
+	            								data.getStatus());
+	            						};
+	            					};
+	                        	}
+	                        }
+	                    };
+					};
+				};
+
+			};
+			// prepends the doc type <!DOCTYPE html>
+			html.setPrependDocType(true);
+			createHtmlFile(html.toHtmlString());
+			System.out.println(html.toHtmlString());
+		};
+		
+		private void createHtmlFile(String htmlString) {
+			File file = new File("report.html");
+			try {
+				FileUtils.writeStringToFile(file, htmlString, false);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+
---
+++ ReportUtility/src/main/java/org/sam/report/Main.java
@@ -0,0 +1,68 @@
+package org.sam.report;
+
+
+import org.dom4j.io.SAXReader;
+//import org.pack.businesslib.HtmlGenerator;
+//import org.pack.data.ReportData;
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+import java.net.URL;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Java client program to demonstrate how to access Hudson remote API.
+ *
+ * @author Kohsuke Kawaguchi
+ * @see http://hudson.gotdns.com/wiki/display/HUDSON/Remote+access+API
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // every Hudson model object exposes the .../api/xml, but in this example
+        // we'll just take the root object as an example
+       // URL url = new URL("http://deadlock.netbeans.org/hudson/api/xml");
+    	 URL url = new URL("http://localhost:8080/api/xml");
+    	 
+        // if you are calling security-enabled Hudson and
+        // need to invoke operations and APIs that are protected,
+        // consult the 'SecuredMain" class
+        // in this package for an example using HttpClient.
+
+        // read it into DOM.
+        Document dom = new SAXReader().read(url);
+        
+        List<ReportData> reportDatas = new ArrayList<>();
+
+        // scan through the job list and print its status
+        for( Element job : (List<Element>)dom.getRootElement().elements("job")) {
+        	
+        	//ArrayList<Element> a=new ArrayList<Element>();
+        	//a.add(job);
+        	//System.out.println(job);
+        	
+        	ReportData data = new ReportData(job.elementText("name"), 
+        			job.elementText("color").equals("blue") ? "Pass" : "Fail");
+        	
+        	
+/*            System.out.println(String.format("Name:%s\t\tStatus:%s",
+                job.elementText("name"), job.elementText("color")));
+            
+            String color= "blue";
+			if(job.elementText("color").equals(color)){
+				System.out.println("Pass");
+			}
+			else {
+				System.out.println("Fail");
+			}*/
+        	reportDatas.add(data);
+        }
+        
+        HtmlGenerator htmlGenerator =  new HtmlGenerator();
+        htmlGenerator.generateReport(reportDatas);
+     
+        
+    }
+}
